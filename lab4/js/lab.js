13c75cfae8cada4b0c0a2ce61124eb9b35acc719
HalleFouche <hafouche@ucsc.edu>
2025-04-24 08:52:29 -0700

    edits

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// Your Name <hafouche@ucsc.edu
+// April 21 2025
 
-// Constants
+// Display game options to player (rock, paper, scissors)
+// Get player input
+// Randomly select computer choice
+// compare player and computer choices
+    // if same, it's a tie
+    // If player wins, display win message
+    // If computer wins, display lose message
+// Ask if player wants to play again
 
-// Functions
+// Changing a Tire
+// Park the car safely
+    // Find a flat surface
+    // Turn on hazard lights
+    // Apply parking break
+// gather necessary tools
+    // Spare tire
+    // Jack
+    // lug wrench
+// Loosen lug nuts slightly
+// Jack up the car
+// Remove lug nuts and take off the flat tire
+// Mount the spare tire
+// Tighten the lug nuts by hand
+// Lower the car using the jack
+// fully tighten the lug nuts with the wrench
+// put away tools and flat tire
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
